fc57e181f9f5a62508511c8dc4ff7b0b9bc76286
livingrockrises <chirag@biconomy.io>
2021-09-18 16:29:31 +0530

    hardhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+const walletUtils = require("./walletUtils");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const fs = require('fs');
+const infuraKey = fs.readFileSync(".infura").toString().trim();
+const alchemyKey = fs.readFileSync(".alchemy").toString().trim();
+const blockvigilKey = fs.readFileSync(".blockvigil").toString().trim();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  //expand to required versions
+  solidity: "0.8.4",
+
+  networks: {
+    hardhat: {
+      initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+    },
+    ropsten: {
+      url:`https://ropsten.infura.io/v3/${infuraKey}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : walletUtils.makeKeyList(),
+    },
+    //add more networks here
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
