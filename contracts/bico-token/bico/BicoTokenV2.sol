c4d148e3ce13a30e32070e68463fea0227224a5d
livingrockrises <chirag@biconomy.io>
2021-10-01 19:01:54 +0530

    draft dummy upgrade contract

---
+++ contracts/bico-token/bico/BicoTokenV2.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+//import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./BicoTokenImplementation.sol";
+
+contract BicoTokenV2 is BicoTokenImplementation {
+   
+}
