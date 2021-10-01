fbb6904e3fd8b352e52a542a52e3db4df157f95a
livingrockrises <chirag@biconomy.io>
2021-10-01 21:27:36 +0530

    working upgrade test example

--- contracts/bico-token/bico/BicoTokenV2.sol
+++ contracts/bico-token/bico/BicoTokenV2.sol
@@ -5,6 +5,7 @@ import "./BicoTokenImplementation.sol";
 
 contract BicoTokenV2 is BicoTokenImplementation {
    
+    bool internal _initializedV2;
     /// @notice Address which may mint new tokens
     address public minter;
 
@@ -17,18 +18,24 @@ contract BicoTokenV2 is BicoTokenImplementation {
     /// @notice Cap on the percentage of totalSupply that can be minted at each mint
     uint8 public constant mintCap = 2;
 
-    function initialize(address trustedForwarder) public initializer {
-        __BicoTokenImplementation_init(trustedForwarder); // Do not forget this call!
+    function initializeV2() external{
+        require(!_initializedV2, "BicoTokenV2: cpntract is already initialized");
+        //__BicoTokenImplementation_init(trustedForwarder); // Do not forget this call!
         minter = msg.sender;
         mintingAllowedAfter = 0;
+        _initializedV2 = true;
     }
     
+    //review
+    //uselss?
     function __BicotokenV2_init_(address trustedForwarder) internal initializer {
        __BicoTokenImplementation_init(trustedForwarder);
        minter = msg.sender;
        mintingAllowedAfter = 0;
     }
    
+    //review
+    //useless?
     function __BicotokenV2_init_unchained() internal initializer {
         minter = msg.sender;
         mintingAllowedAfter = 0;
