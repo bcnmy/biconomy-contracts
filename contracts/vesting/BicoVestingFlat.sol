11b69f9833d75c0426486ea334e5c9e85ddda1d9
livingrockrises <chirag@biconomy.io>
2021-11-25 18:46:45 +0530

    remediation commit

--- contracts/vesting/BicoVestingFlat.sol
+++ contracts/vesting/BicoVestingFlat.sol
@@ -4,7 +4,6 @@
 
 // SPDX-License-Identifier: MIT
 
-
 pragma solidity >=0.6.0 <0.8.0;
 
 /*
@@ -27,70 +26,6 @@ abstract contract Context {
         return msg.data;
     }
 }
-
-// File @openzeppelin/contracts/security/ReentrancyGuard.sol@v3.4.2
-
-pragma solidity >=0.6.0 <0.8.0;
-
-/**
- * @dev Contract module that helps prevent reentrant calls to a function.
- *
- * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
- * available, which can be applied to functions to make sure there are no nested
- * (reentrant) calls to them.
- *
- * Note that because there is a single `nonReentrant` guard, functions marked as
- * `nonReentrant` may not call one another. This can be worked around by making
- * those functions `private`, and then adding `external` `nonReentrant` entry
- * points to them.
- *
- * TIP: If you would like to learn more about reentrancy and alternative ways
- * to protect against it, check out our blog post
- * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
- */
-abstract contract ReentrancyGuard {
-    // Booleans are more expensive than uint256 or any type that takes up a full
-    // word because each write operation emits an extra SLOAD to first read the
-    // slot's contents, replace the bits taken up by the boolean, and then write
-    // back. This is the compiler's defense against contract upgrades and
-    // pointer aliasing, and it cannot be disabled.
-
-    // The values being non-zero value makes deployment a bit more expensive,
-    // but in exchange the refund on every call to nonReentrant will be lower in
-    // amount. Since refunds are capped to a percentage of the total
-    // transaction's gas, it is best to keep them low in cases like this one, to
-    // increase the likelihood of the full refund coming into effect.
-    uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
-
-    uint256 private _status;
-
-    constructor() {
-        _status = _NOT_ENTERED;
-    }
-
-    /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * Calling a `nonReentrant` function from another `nonReentrant`
-     * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and make it call a
-     * `private` function that does the actual work.
-     */
-    modifier nonReentrant() {
-        // On the first call to nonReentrant, _notEntered will be true
-        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
-
-        // Any calls to nonReentrant after this point will fail
-        _status = _ENTERED;
-
-        _;
-
-        // By storing the original value once again, a refund is triggered (see
-        // https://eips.ethereum.org/EIPS/eip-2200)
-        _status = _NOT_ENTERED;
-    }
-}
-
 
 
 // File @openzeppelin/contracts/token/ERC20/IERC20.sol@v3.4.2
@@ -1074,6 +1009,7 @@ abstract contract AccessProtected is Context, Ownable {
      * @param enabled - Enable/Disable Admin Access
      */
     function setAdmin(address admin, bool enabled) external onlyOwner {
+        require(admin != address(0), "Admin address can not be zero");
         _admins[admin] = enabled;
         emit AdminAccessSet(admin, enabled);
     }
@@ -1111,7 +1047,7 @@ pragma abicoder v2;
 
 
 
-contract BicoVesting is AccessProtected, Pausable, ReentrancyGuard {
+contract BicoVesting is AccessProtected, Pausable {
     using SafeMath for uint256;
     using Address for address;
     address public tokenAddress;
@@ -1141,6 +1077,7 @@ contract BicoVesting is AccessProtected, Pausable, ReentrancyGuard {
     event Revoked(address _beneficiary);
 
     constructor(address _tokenAddress) {
+        require(_tokenAddress != address(0),"Token address can not be zero");
         tokenAddress = _tokenAddress;
     }
 
@@ -1153,15 +1090,11 @@ contract BicoVesting is AccessProtected, Pausable, ReentrancyGuard {
         uint64 _endTime
     ) public onlyAdmin {
         require(!claims[_beneficiary].isActive, "CLAIM_ACTIVE");
-        require(_endTime >= _startTime, "INVALID_TIME");
+        require(_endTime > _startTime, "INVALID_TIME");
+        require(_endTime != 0, "INVALID_TIME");
+        require(_startTime > _unlockTime, "INVALID_TIME");
         require(_beneficiary != address(0), "INVALID_ADDRESS");
         require(_vestAmount > 0, "INVALID_AMOUNT");
-        //review
-        //should probably use IERC20 interface instead of ERC20 import?
-        //need for safe transfer?
-
-        //notice
-        //Admin needs to give prior apporve tokens to this contract
         require(
             ERC20(tokenAddress).allowance(msg.sender, address(this)) >=
                 (_vestAmount.add(_unlockAmount)),
@@ -1170,7 +1103,7 @@ contract BicoVesting is AccessProtected, Pausable, ReentrancyGuard {
         ERC20(tokenAddress).transferFrom(
             msg.sender,
             address(this),
-            _vestAmount
+            _vestAmount.add(_unlockAmount)
         );
         Claim memory newClaim = Claim({
             isActive: true,
@@ -1275,24 +1208,25 @@ contract BicoVesting is AccessProtected, Pausable, ReentrancyGuard {
         return unclaimedAmount;
     }
 
-    function claim() external whenNotPaused nonReentrant {
+    function claim() external whenNotPaused {
         address beneficiary = msg.sender;
         Claim memory _claim = claims[beneficiary];
         require(_claim.isActive, "CLAIM_INACTIVE");
         uint256 unclaimedAmount = claimableAmount(beneficiary);
         ERC20(tokenAddress).transfer(beneficiary, unclaimedAmount);
-        _claim.amountClaimed = _claim.amountClaimed + unclaimedAmount;
+        _claim.amountClaimed = _claim.amountClaimed.add(unclaimedAmount);
         if (_claim.amountClaimed == _claim.vestAmount) _claim.isActive = false;
         claims[beneficiary] = _claim;
         emit Claimed(beneficiary, unclaimedAmount);
     }
 
     function revoke(address beneficiary) external onlyAdmin {
+        require(claims[beneficiary].isActive != false, "Already invalidated");
         claims[beneficiary].isActive = false;
         emit Revoked(beneficiary);
     }
 
-    function withdrawTokens(address wallet, uint256 amount) external onlyOwner nonReentrant {
+    function withdrawTokens(address wallet, uint256 amount) external onlyOwner {
         require(amount > 0, "Nothing to withdraw");
         ERC20(tokenAddress).transfer(wallet, amount);
     }
